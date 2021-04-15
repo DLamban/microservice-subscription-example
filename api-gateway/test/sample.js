fef72ae93a71a842b400d27e75019b72f1e0a64a
Your Name <you@example.com>
2021-04-15 18:50:36 +0200

    testing

--- api-gateway/test/sample.js
+++ api-gateway/test/sample.js
@@ -6,6 +6,21 @@ const app = require("../src/server");
 chai.use(chaiHttp);
 chai.should();
 
+
+// function authenticateUser() {
+//   return new Promise((resolve, reject) => {
+
+//     request(app)
+//       .get('/api/getAdminToken')
+//       .end((err, res) => {
+//         if (err) {
+//           return reject(err);
+//         }
+//         return resolve(res.body.token);
+//       });
+//   });
+// }
+
 describe("API /healthz", () => {
   it("it should return 200", done => {
     chai
@@ -19,14 +34,40 @@ describe("API /healthz", () => {
 });
 
 describe("API /api/getAdminToken", () => {
-  it("it should return an token", done => {
+  it("it should return a token", done => {
+    chai
+    // .request(app)
+    // .get("/api/getAdminToken")
+    // .end((err, res) => {
+
+    .request(app)
+      .get('/api/getAdminToken')
+      .end((err, res) => {
+        if (err) {
+          done();//return reject(err);
+        }
+        chai.request(app)
+          .get('api/getAllSubscriptions')
+          .end((err, res) => {
+            done()
+          });
+        //})
+        // res.should.have.status(200);
+        // res.should.to.be.json;
+        // console.log(res.body)
+      });
+  });
+});
+
+describe("API /api/getAdminToken", () => {
+  it("it should return a token", done => {
     chai
       .request(app)
       .get("/api/getAdminToken")
       .end((err, res) => {
         res.should.have.status(200);
-        res.should.to.be.json;        
-        res.json.body.token.should.exist;
+        res.should.to.be.json;
+        console.log(res.body)
         done();
       });
   });
